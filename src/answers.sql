e16bac3abaadc5b64a02d596810c2df7a85c5727
Andre Gallegos <andre.gallegos@tecsup.edu.pe>
2024-11-26 12:00:40 -0500

    Completing pending inquiries

--- src/answers.sql
+++ src/answers.sql
@@ -34,7 +34,107 @@ ORDER BY
 LIMIT
   5;
 
--- 4
--- 5
--- 6
--- 7
+-- 4 Three supervisors with the most amount of employees they are in charge.
+SELECT
+  e.supervisor_id,
+  COUNT(e.*) AS count_number
+FROM
+  employees e
+WHERE
+  e.supervisor_id IS NOT NULL
+GROUP BY
+  e.supervisor_id
+ORDER BY
+  count_number DESC
+LIMIT
+  3;
+
+-- 5 How many offices are in the state of Colorado (United States).
+SELECT
+  COUNT(o.*) AS list_of_office
+FROM
+  offices o
+WHERE
+  state_id = (
+    SELECT
+      s.id
+    FROM
+      states s
+    WHERE
+      s.name = 'Colorado'
+  );
+
+-- 6 The name of the office with its number of employees ordered in a desc.
+SELECT
+  o.name,
+  COUNT(e.*) AS count
+FROM
+  employees e
+  INNER JOIN public.offices o ON o.id = e.office_id
+GROUP BY
+  o.name
+ORDER BY
+  count DESC;
+
+-- 7 The office with the most and the least number of employees
+WITH employees_count AS (
+  -- this is a subquery that will count the number of employees in each office
+  SELECT
+    o.address,
+    COUNT(e.*) AS count
+  FROM
+    employees e
+    INNER JOIN offices o ON o.id = e.office_id
+  GROUP BY
+    o.address
+),
+-- this is a subquery that will get the office with the most employees based on the last subquery
+office_max AS (
+  SELECT
+    ec.address,
+    ec.count
+  FROM
+    employees_count ec
+  ORDER BY
+    ec.count DESC
+  LIMIT
+    1
+), -- this is a subquery that will get the office with the least employees based on the last subquery
+office_min AS (
+  SELECT
+    ec.address,
+    ec.count
+  FROM
+    employees_count ec
+  ORDER BY
+    ec.count
+  LIMIT
+    1
+)
+SELECT
+  *
+FROM
+  office_max
+UNION
+SELECT
+  *
+FROM
+  office_min;
+
+-- 8 Show the uuid of the employee, first_name and lastname combined, email, job_title, the name of the office they belong to,
+-- the name of the country, the name of the state and the name of the boss (boss_name)
+SELECT
+  e.uuid,
+  e.first_name || ' ' || e.last_name AS full_name,
+  e.email,
+  e.job_title,
+  o.name AS company,
+  c.name AS country,
+  s.name AS state,
+  e2.first_name AS boss_name
+FROM
+  employees e
+  INNER JOIN offices o ON o.id = e.office_id
+  INNER JOIN countries c ON c.id = o.country_id
+  INNER JOIN states s ON s.id = o.state_id
+  INNER JOIN employees e2 ON e2.id = e.supervisor_id;
